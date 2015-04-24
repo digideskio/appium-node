3d242f0e7db850dc58b3609e1c79c2e5795af439
Daniel Tolbert <dtolbert@bandwidth.com>
2015-04-24 10:19:43 -0400

    Complex demo

--- helpers/caps.js
+++ helpers/caps.js
@@ -17,6 +17,15 @@ exports.android18 = {
   app: undefined // will be set later
 };
 
+exports.iOSDevice = {
+  'appium-version': '1.3',
+  platformName: 'iOS',
+  platformVersion: '8.1',
+  //deviceName: 'iPhone Simulator',
+  deviceName: 'iPhone'
+};
+
+
 exports.android19 = {
   browserName: '',
   'appium-version': '1.3',
